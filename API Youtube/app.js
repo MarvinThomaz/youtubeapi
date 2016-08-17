222564e2e75327e20f057b346c9d441bcfbcb56d
MarvinThomaz <marvinthomaz@gmail.com>
2016-08-17 06:49:47 -0300

    Ajustes gerais

--- API Youtube/app.js
+++ API Youtube/app.js
@@ -1,34 +1,42 @@
-var _objeto = null,
-    _data = { part: 'contentDetails', forUsername: '1taldemarvin', key: 'AIzaSyBM3XlO94bHBN6P8AbfG4w4Z-JSWOhDYrM' };
-
-function sucesso(objeto){
-    listarPlaylists(objeto.items);
-}
-
-function erro(objeto){
-    console.log(objeto.responseJSON.error.message);
-}
-
-function listarPlaylists(lista){
-    var tabela = document.getElementById("tabela");
-    var itens = "";
-    var tamanho = lista.length;
+var YTVideo = function(){
+    var data = { part: 'contentDetails', forUsername: '1taldemarvin', key: 'AIzaSyBM3XlO94bHBN6P8AbfG4w4Z-JSWOhDYrM' };
     
-    for(var i = 0; i < tamanho; i ++){
-        itens += "<div class='row'><a href='#' onclick='listarVideos(this);' item-id='" + lista[i].propriedadedeid + "'>" + lista[i].propriedadedenome + "</a></div>";
+    function sucesso(objeto){
+        listarPlaylists(objeto.items);
+    }
+    
+    function erro(objeto){
+        console.log(objeto.responseJSON.error.message);
+    }
+    
+    function listarPlaylists(lista){
+        var _tabela = $('.table');
+        var _itens = "";
+        var _tamanho = lista.length;
+        
+        for(var i = 0; i < _tamanho; i ++){
+            _itens += "<div class='row'><a href='#' onclick='listarVideos(this);' item-id='" + lista[i].propriedadedeid + "'>" + lista[i].propriedadedenome + "</a></div>";
+        }
+        
+        _tabela.html("");
+        _tabela.html(_itens);
+    }
+    
+    function listarVideos(item){
+        //requisição para listar os vídeos com o id da playlist que seria o curso
+    }
+    
+    this.requisitar = function(){
+        $.ajax({
+            type:'GET',
+            url: 'https://www.googleapis.com/youtube/v3/channels',
+            data: data,
+            success: sucesso,
+            error: erro
+        });
     }
 }
 
-function listarVideos(item){
-    //requisição para listar os vídeos com o id da playlist que seria o curso
-}
+var yt = new YTVideo();
 
-$(document).ready(function(){ 
-    $.ajax({
-        type:'GET',
-        url: 'https://www.googleapis.com/youtube/v3/channels',
-        data: _data,
-        success: sucesso,
-        error: erro
-    });
-});
+$(document).ready(yt.requisitar);

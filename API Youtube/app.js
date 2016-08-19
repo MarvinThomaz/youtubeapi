1ecd4445878acd687056de74dd216a7f1cc3e2ea
MarvinThomaz <marvinthomaz@gmail.com>
2016-08-19 07:04:06 -0300

    ajustes de replicação de codigo

--- API Youtube/app.js
+++ API Youtube/app.js
@@ -30,8 +30,70 @@ var YTVideo = function () {
         _tabela.html("");
         _tabela.html(_itens);
         _tabela.find('[playlist-items]').hide();
-        _tabela.find('[item-id]').click(requisitarVideos);
+        _tabela.find('[item-id]').click(requisitarPublicos);
         _tabela.find('[more]').click(requisitarPrivados);
+    }
+
+    function listarVideos(lista, show) {
+        esvaziarContainers();
+
+        var count = lista.length;
+
+        if(count > 0) {
+            var elemento = $("[playlist-items=" + lista[0].snippet.playlistId + "] [items]");
+
+            elemento.html("");
+
+            for (var i = 0; i < count; i++)
+                elemento.append('<p><strong>' + lista[i].snippet.title + '</strong></p><p><iframe width="420" height="315" src="https://www.youtube.com/embed/' + lista[i].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe></p>');
+
+            elemento.parent().show();
+
+            if(show)
+                elemento.parent().find('[more]').show();
+            else
+                elemento.parent().find('[more]').hide();
+        }
+    }
+
+    function listarPublicos(lista) {
+        var colecao = lista.items, count = colecao.length, array = [];
+
+        for (var i = 0; i < count; i++)
+            if(colecao[i].snippet.title != private)
+                array.push(colecao[i]);
+
+        listarVideos(array, true);
+    }
+
+    function listarPrivados(lista) {
+        var colecao = lista.items, count = colecao.length, array = [];
+
+        for (var i = 0; i < count; i++)
+            if(colecao[i].snippet.title == private)
+                array.push(colecao[i]);
+
+        listarVideos(array, false);
+    }
+
+    function requisitarPublicos() {
+        requisitarVideos(listarPublicos, $(this).attr("item-id"));
+    }
+
+    function requisitarPrivados() {
+        requisitarVideos(listarPrivados, $(this).attr("more"));
+    }
+
+    function requisitarVideos(sucesso, id) {
+        var _data = { part: 'snippet', maxResults: 10, playlistId: id, key: key };
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://www.googleapis.com/youtube/v3/playlistItems',
+            data: _data,
+            success: sucesso,
+            error: erro
+        });
     }
 
     function requisitarPlaylists(channel){
@@ -42,69 +104,6 @@ var YTVideo = function () {
             url: 'https://www.googleapis.com/youtube/v3/playlists',
             data: _data,
             success: listarPlaylists,
-            error: erro
-        });
-    }
-
-    function listarVideos(lista) {
-        esvaziarContainers();
-
-        var count = lista.items.length;
-
-        if(count > 0) {
-            var elemento = $("[playlist-items=" + lista.items[0].snippet.playlistId + "] [items]");
-
-            elemento.html("");
-
-            for (var i = 0; i < count; i++) {
-                if(lista.items[i].snippet.title != private)
-                    elemento.append('<p><strong>' + lista.items[i].snippet.title + '</strong></p><p><iframe width="420" height="315" src="https://www.youtube.com/embed/' + lista.items[i].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe></p>')
-            }
-
-            elemento.parent().show();
-            elemento.parent().find('[more]').show();
-        }
-    }
-
-    function requisitarVideos() {
-        var _data = { part: 'snippet', maxResults: 10, playlistId: $(this).attr('item-id'), key: key };
-
-        $.ajax({
-            type: 'GET',
-            url: 'https://www.googleapis.com/youtube/v3/playlistItems',
-            data: _data,
-            success: listarVideos,
-            error: erro
-        });
-    }
-
-    function listarPrivados(lista) {
-        
-        var count = lista.items.length;
-
-        if(count > 0) {
-            var elemento = $("[playlist-items=" + lista.items[0].snippet.playlistId + "] [private]");
-
-            elemento.html("");
-
-            for (var i = 0; i < count; i++) {
-                if(lista.items[i].snippet.title == private)
-                    elemento.append('<p><strong>' + lista.items[i].snippet.title + '</strong></p><p><iframe width="420" height="315" src="https://www.youtube.com/embed/' + lista.items[i].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe></p>')
-            }
-
-            elemento.parent().show();
-            elemento.parent().find('[more]').hide();
-        }
-    }
-
-    function requisitarPrivados() {
-        var _data = { part: 'snippet', maxResults: 10, playlistId: $(this).attr('more'), key: key };
-
-        $.ajax({
-            type: 'GET',
-            url: 'https://www.googleapis.com/youtube/v3/playlistItems',
-            data: _data,
-            success: listarPrivados,
             error: erro
         });
     }
